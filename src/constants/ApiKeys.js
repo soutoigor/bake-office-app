5400cec8a9031410f3ecfc298b17abb3a2ed73f4
Igor Souto <igor_souto@outlook.com>
2019-08-01 02:00:53 -0300

    feat(receip): add create receip page and logic
    Add receip page layout form.
    Add receip logic, validations, post to API.
    Install and connect to Firebase.

---
+++ src/constants/ApiKeys.js
@@ -0,0 +1,11 @@
+export default {
+  firebaseConfig: {
+    apiKey: "AIzaSyDO95As-75r5qK2RLjbv6xx9vHeZUcPafE",
+    authDomain: "bake-office.firebaseapp.com",
+    databaseURL: "https://bake-office.firebaseio.com",
+    projectId: "bake-office",
+    storageBucket: "bake-office.appspot.com",
+    messagingSenderId: "479154046154",
+    appId: "1:479154046154:web:945008389234c10e"
+  },
+}
